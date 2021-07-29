de24ee55f75f03d2375ec081b347c31f969a0f2e
AlexVater <alexsander.vater@gmail.com>
2021-07-29 10:20:02 -0300

    Calculadora

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Calculator from "./main/Calculator";
-import reportWebVitals from './reportWebVitals';
+import App from "./views/App";
 
 ReactDOM.render(
-    <div>
-        <h1>Calculadora</h1>
-        <Calculator />
-    </div>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
